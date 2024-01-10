32bf345957b40961f28e999c4bb2be1ab9f9ce08
이재필 <harry_jp0830@naver.com>
2024-01-10 12:18:48 +0900

    second solve 2525

--- 기초/조건문/oven_clock_2525.py
+++ 기초/조건문/oven_clock_2525.py
@@ -4,10 +4,7 @@ time_to_cook = int(input()) # 요리하는 데 필요한 시간
 total_minute = minute + time_to_cook
 while total_minute >= 60:
     total_minute = total_minute - 60
-    if hour == 23:
-        hour = 0
-    else:
-        hour = hour + 1
+    hour = (hour + 1) % 24
 
 minute = total_minute
 print(hour, minute)
