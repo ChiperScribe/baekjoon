f88b57aed4d6e02a0566362e0b9a5a370cbfbfbf
이재필 <harry_jp0830@naver.com>
2024-01-10 12:21:53 +0900

    final solve 2525

--- 기초/조건문/oven_clock_2525.py
+++ 기초/조건문/oven_clock_2525.py
@@ -2,9 +2,7 @@ hour, minute = map(int, input().split())
 time_to_cook = int(input()) # 요리하는 데 필요한 시간
 
 total_minute = minute + time_to_cook
-while total_minute >= 60:
-    total_minute = total_minute - 60
-    hour = (hour + 1) % 24
+hour = (hour + (total_minute // 60)) % 24
+minute = total_minute % 60
 
-minute = total_minute
 print(hour, minute)
